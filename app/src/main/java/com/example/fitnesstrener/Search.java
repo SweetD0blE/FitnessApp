cc182b1aa5b12cf22ba6decd18fc96c5c164ce18
Илья Терентьев <cam1234251@mail.ru>
2022-05-16 17:44:10 +0300

    Update all icons exercise

--- app/src/main/java/com/example/fitnesstrener/Search.java
+++ app/src/main/java/com/example/fitnesstrener/Search.java
@@ -1,30 +1,33 @@
 package com.example.fitnesstrener;
 
-import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.SearchView;
-import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.DividerItemDecoration;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
+
 public class Search extends AppCompatActivity {
 
-    private ItemAdapter itemAdapter;
-    private SearchView searchView;
-    public List<Item> itemList;
-    private RecyclerView recyclerView;
+    RecyclerView recyclerView;
+    RecyclerAdapter recyclerAdapter;
     ImageButton imageButtonReturn1;
+
+    List<String> moviesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,29 +44,29 @@ public class Search extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL));
+        moviesList = new ArrayList<>();
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemViewCacheSize(24);
+        recyclerView.setDrawingCacheEnabled(true);
+        recyclerAdapter = new RecyclerAdapter(moviesList);
+        recyclerView.setAdapter(recyclerAdapter);
 
-        searchView = findViewById(R.id.searchView);
-        searchView.clearFocus();
+        moviesList.add("Скручивания");
+        moviesList.add("Планка");
+        moviesList.add("Отжимания");
+        moviesList.add("Подтягивания");
+        moviesList.add("Скручивания");
+        moviesList.add("Скручивания");
 
+    }
 
-        itemList = new ArrayList<>();
-
-        itemList.add(new Item("Skrychivaniya", R.drawable.twisting));
-        itemList.add(new Item("Skrychivaniya", R.drawable.twisting));
-        itemList.add(new Item("Skrychivaniya", R.drawable.twisting));
-        itemList.add(new Item("Skrychivaniya", R.drawable.twisting));
-        itemList.add(new Item("Skrychivaniya", R.drawable.twisting));
-        itemList.add(new Item("Skrychivaniya", R.drawable.twisting));
-        itemList.add(new Item("Skrychivaniya", R.drawable.twisting));
-
-        itemAdapter = new ItemAdapter(itemList);
-        recyclerView.setAdapter(itemAdapter);
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -72,28 +75,13 @@ public class Search extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                filterList(newText);
-                return true;
+                recyclerAdapter.getFilter().filter(newText);
+                return false;
             }
         });
 
+
+
+        return super.onCreateOptionsMenu(menu);
     }
-
-    private void filterList(String text) {
-        List<Item> filteredList = new ArrayList<>();
-        for (Item item : itemList){
-            if (item.getItemName().toLowerCase().contains(text.toLowerCase())){
-                filteredList.add(item);
-            }
-        }
-
-        if (filteredList.isEmpty()){
-            Toast.makeText(this,"Упражнение не найдено", Toast.LENGTH_SHORT).show();
-        }else{
-            itemAdapter.setFilteredList(filteredList);
-        }
-
-    }
-
-
 }
